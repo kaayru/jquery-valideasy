176638b8583eddb35a02249395e6dc0a73334045
Vincent Ballut <kaayru@gmail.com>
2013-10-18 00:21:21 +0200

    Added examples to the GH page

---
+++ javascripts/jquery-autoclear.js
@@ -0,0 +1,33 @@
+/* ------------------------------------------------------------------------
+	Class: autoClear
+	Use: Clears input fields when clicked
+	Author: Vincent Ballut
+	Version: 1.0
+	Dependency: jQuery 1.9.1
+------------------------------------------------------------------------- */
+
+(function($) {
+    $.fn.autoClear = function() { 
+    	var element = $(this);
+    	var defaultValue = element.attr('title');
+
+    	element.on('focus', function() {
+    		var value = $(this).val();
+    		if(defaultValue == value) {
+    			$(this).val('');
+    		}
+    		$(this).addClass('focus_input');
+    	});
+
+    	element.on('blur', function() {
+    		var value = $(this).val();
+    		if(value == '' || value == defaultValue) {
+    			$(this).val(defaultValue);
+    			$(this).removeClass('modified_input');
+    		} else {
+    			$(this).addClass('modified_input');
+    		}
+    		$(this).removeClass('focus_input');
+    	});
+    };
+})(jQuery);
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+    $('.autoclear').each(function() {
+    	$(this).autoClear();
+    });
+
+    $('form').submit(function(){
+    	var formIsValid = $(this).valideasy();
+
+    	if(formIsValid) {
+    		$(this).find('.alert-success').fadeIn();
+    	} else {
+    		$(this).find('.alert-success').fadeOut();
+    	}
+    	return false;
+	});
+});
